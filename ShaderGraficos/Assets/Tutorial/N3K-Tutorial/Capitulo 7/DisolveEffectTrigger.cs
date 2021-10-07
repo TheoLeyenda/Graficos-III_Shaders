c141c3ad12275dce77c86c4512ce0773fd455b71
Theo Leyenda <theoleyenda1997@gmail.com>
2021-10-07 17:41:22 -0300

    Capitulo 7

---
+++ ShaderGraficos/Assets/Tutorial/N3K-Tutorial/Capitulo 7/DisolveEffectTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisolveEffectTrigger : MonoBehaviour
+{
+    public Material disolveMaterial;
+    public float speed, max;
+    public string currentY_parameterName = "_DisolveY";
+    private float currentY, startTime;
+
+    private void Update()
+    {
+        if (currentY < max)
+        {
+            disolveMaterial.SetFloat(currentY_parameterName, currentY);
+            currentY += Time.deltaTime * speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TriggerEffect();
+        }
+    }
+
+    private void TriggerEffect()
+    {
+        startTime = Time.time;
+        currentY = 0;
+    }
+}
